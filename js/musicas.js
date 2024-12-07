239d70ff89da5ccea9d414969fb1aee3ca4c3258
Fredy Antonio Pelaez C <103303538+Fredy-AP@users.noreply.github.com>
2024-12-07 16:07:58 -0500

    Update musicas.js

--- js/musicas.js
+++ js/musicas.js
@@ -36,7 +36,7 @@ const songs=[
     // img: "https://github.com/Brandon328/music-player/blob/master/assets/img/elements.jpg"
   },
   {
-    id: "song8",
+    id: "song5",
     name: "La mujer perfecta",
     artist: "Kurt",
     url: "assets/musicas/playa.mp3",
